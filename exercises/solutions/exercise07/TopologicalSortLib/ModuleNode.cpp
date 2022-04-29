aa0eec97ccbeab88f6ab2db9e37ff608f637ee3b
PeterSommerlad <peter.cpp@sommerlad.ch>
2022-04-29 16:48:11 +0200

    more solutions

---
+++ exercises/solutions/exercise07/TopologicalSortLib/ModuleNode.cpp
@@ -0,0 +1,48 @@
+#include "ModuleNode.h"
+#include <sstream>
+
+
+ModuleNode::AllModules::AllModules(std::istream &input) {
+  std::string line{};
+  while (std::getline(input, line)) {
+    std::istringstream is { line };
+    std::string name;
+    if (is >> name) {
+      auto current = insert(name);
+      while (is >> name) {
+        current->addRequired(name, *this);
+      }
+    }
+  }
+  sort(begin(all),end(all));
+}
+
+ModuleNodePtr ModuleNode::AllModules::insert(std::string modulename) & {
+  auto iter = this->find(modulename);
+  if (iter != end(all)) {
+    return *iter; // already there
+  }
+  auto newmodule = std::make_shared<ModuleNode>(modulename);
+  all.push_back(newmodule);
+  return newmodule;
+}
+
+void ModuleNode::AllModules::printSchedule(std::ostream &out) const {
+  unsigned semester{0u};
+  auto last = begin(all);
+  while(last != end(all)){
+    ++semester;
+    out << semester << " : ";
+    auto const next = partition_point(last,end(all),[semester](auto const &mp){ return mp && mp->semester == semester;});
+    for_each(last,next,[&out](auto const &mp){ if (mp) out << mp->name << ", ";});
+    out << '\n';
+    last = next;
+  }
+}
+
+void print_semester_topological(std::istream &in, std::ostream & out){
+  ModuleNode::AllModules theModules{in};
+  theModules.scheduleAll();
+  theModules.printSchedule(out);
+}
+
---
+++ exercises/solutions/exercise07/TopologicalSortLib/ModuleNode.h
@@ -0,0 +1,100 @@
+#ifndef MODULENODE_H_
+#define MODULENODE_H_
+
+
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <vector>
+#include <memory>
+#include <iosfwd>
+
+using ModuleNodePtr = std::shared_ptr<struct ModuleNode>;
+struct cyclic_dependency{};
+struct ModuleNode {
+  std::string name{};
+  std::vector<ModuleNodePtr> required{};
+  unsigned semester{unknown_semester};
+  struct AllModules {
+    std::vector<ModuleNodePtr> all{};
+    AllModules() = default;
+    AllModules(std::istream &in);
+
+    bool contains(std::string modulename) const {
+      return any_of(cbegin(all), cend(all), [modulename](auto const &nodeptr){
+        return nodeptr && nodeptr->name == modulename;
+      });
+    }
+    auto find(std::string modulename) const {
+      return find_if(cbegin(all), cend(all), [modulename](auto const & nodeptr){
+        return nodeptr && nodeptr->name == modulename;
+      });
+    }
+    auto schedule(unsigned semester) & {
+      auto res = stable_partition(begin(all),end(all),[semester](auto const & m){ return m && m->isReady(semester);});
+      for_each(begin(all),res,[semester](auto const& m){ if (m && m->semester == unknown_semester) m->semester = semester;});
+      return res;
+    }
+    ModuleNodePtr insert(std::string modulename) &;
+    void scheduleAll() & {
+      unsigned semester{0u};
+      auto last = begin(all);
+      while(last != end(all)){
+        auto const next = schedule(++semester);
+        if (next == last) throw cyclic_dependency{};
+        last = next;
+      }
+    }
+    void printSchedule(std::ostream&) const;
+  };
+
+  ModuleNode(std::string modulename)
+  : name{modulename}{}
+  bool isReady(unsigned semester){
+    return all_of(begin(required),end(required),[semester](auto const &nodeptr){
+      return nodeptr && nodeptr->semester < semester;
+    });
+  }
+  auto operator<=>(ModuleNode const &other) const  {
+    return name <=> other.name;
+  }
+  bool operator==(ModuleNode const &other) const  {
+    return name == other.name;
+  }
+  void addRequired(std::string modulename, ModuleNode::AllModules &theModules) & {
+    required.push_back(theModules.insert(modulename));
+  }
+  bool schedule(unsigned semester) & {
+    if (semester >0 && isReady(semester-1)) {
+      this->semester = semester;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  constexpr static unsigned unknown_semester = -1; // really big
+};
+
+inline
+bool operator==(ModuleNodePtr const &p, std::string const &name){
+  return p && *p == name;
+}
+
+inline
+auto operator<=>(ModuleNodePtr const &p, std::string const &name){
+  if (p) return *p <=> name;
+  return std::strong_ordering::less;
+}
+inline
+bool operator==(ModuleNodePtr const &p, ModuleNodePtr const &r){
+  return p && r && p->name == r->name;
+}
+inline
+auto operator<=>(ModuleNodePtr const &p, ModuleNodePtr const &r){
+  if (p && r) return p->name <=> r->name;
+  return std::strong_ordering::less;
+}
+void print_semester_topological(std::istream &in, std::ostream & out);
+
+
+#endif /* MODULENODE_H_ */
